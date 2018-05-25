e5d03142e0bbc6cf6e0d3005d596b98caffcaf67
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-05-25 09:55:07 -0700

    Completed warm-up 1.

---
+++ warm-up1/main.js
@@ -0,0 +1,15 @@
+var students = [
+ 'JJ',
+ 'Franz',
+ 'Taylor',
+ 'Daryl',
+ 'Jeff',
+ 'Jerome',
+ 'Arthur'
+]
+
+function forEach(list, procedure) {
+    for(var i = 0; i < list.length; i++) {
+      procedure(list[i]);
+    }
+}
