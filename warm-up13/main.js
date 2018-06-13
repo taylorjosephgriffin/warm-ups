05df3f70181ccc74aa6cf7e4bedf371b1fbf2c9c
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-06-13 10:34:27 -0700

    Refactored Warm Up 13 solution to satisfy updated test case.

--- warm-up13/main.js
+++ warm-up13/main.js
@@ -1,17 +1,32 @@
 function padLeft(string, padding, maxLength) {
+  let padString = ''
+  let edgePad = ''
   if (padding.length <= 1) {
-    return padding.repeat((maxLength - string.length)) + string
+    padString = padding.repeat((maxLength - string.length)) + string
   }
-  else {
-    return padding.repeat((maxLength - string.length) - padding.length) + string
+  else if (padding.length > 1){
+    padString = padding.repeat((maxLength - string.length) - padding.length) + string
   }
+  if (padString.length !== maxLength) {
+    edgePad += padString.substring(0, padString.length - string.length - 1)
+    edgePad += padString.substring(padString.length - string.length, padString.length)
+    padString = edgePad
+  }
+  return padString
 }
 
 function padRight(string, padding, maxLength) {
+  let padString = ''
+  let edgePad = ''
   if (padding.length <= 1) {
-    return string + padding.repeat((maxLength - string.length))
+    padString =  string + padding.repeat((maxLength - string.length))
   }
   else {
-    return string + padding.repeat((maxLength - string.length) - padding.length)
+    padString = string + padding.repeat((maxLength - string.length) - padding.length)
   }
+  if (padString.length !== maxLength) {
+    edgePad += padString.substring(0, padString.length - 1)
+    padString = edgePad
+  }
+  return padString
 }
